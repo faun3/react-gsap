8748ce67f23ac786fbf12a5cf0155f8d0f8fd414
faun3 <antonioculcea@gmail.com>
2023-11-03 22:23:17 +0200

    start on practicing scrolling elements and events

---
+++ src/app/scrolling-stuff/page.tsx
@@ -0,0 +1,7 @@
+import PinnedScroll from "@/components/PinnedScroll";
+import style from "./page.module.scss";
+
+const ScrollingStuffPage = () => {
+  return <PinnedScroll />;
+};
+export default ScrollingStuffPage;
---
+++ src/components/PinnedScroll.tsx
@@ -0,0 +1,22 @@
+const PinnedScroll = () => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-activity"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="currentColor"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M3 12h4l3 8l4 -16l3 8h4"></path>
+      </svg>
+    </div>
+  );
+};
+export default PinnedScroll;
